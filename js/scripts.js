ff02e8808e38194fcf02ae15fcf91fbabb909a10
tcederberg <trcederberg@gmail.com>
2023-09-29 14:57:56 -0400

    This commit adds the basic structure of index and scripts js.

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+    {name: 'Squirtle', height: 50, types: ['Water'], abilities: ['Rain dish', 'Torrent']},
+    {name: 'Ninetales', height: 110, types: ['Fire'], abilities: ['Flash fire', 'Drought']},
+    {name: 'Alakazam', height: 150, types: ['Psychic'], abilities: ['Synchronize', 'Inner focus']}
+  ];
