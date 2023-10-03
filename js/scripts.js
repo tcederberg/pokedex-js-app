18d2b5d02ec20ff56ff24e154bc8fb6027e0be3e
tcederberg <trcederberg@gmail.com>
2023-10-03 19:16:18 -0400

    This commit replaces for loop with forEach loop.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,24 @@
 let pokemonList = [
-    {name: 'Squirtle', height: 50, types: ['Water'], abilities: ['Rain dish', 'Torrent']},
-    {name: 'Vulpix', height: 60, types: ['Fire'], abilities: ['Flash fire', 'Drought']},
-    {name: 'Alakazam', height: 150, types: ['Psychic'], abilities: ['Synchronize', 'Inner focus']}
-  ];
-  for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height >100){ //if pokemon's height is over 100cm also print 'Wow that's big
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ' cm) - Wow, that\'s big!<br>');
-    }else {
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ' cm)<br>');
+    {name: 'Squirtle', 
+    height: 50, 
+    types: ['Water'], 
+    abilities: ['Rain dish', 'Torrent']
+    },
+    {name: 'Vulpix', 
+    height: 60, 
+    types: ['Fire'], 
+    abilities: ['Flash fire', 'Drought']
+    },
+    {name: 'Alakazam', 
+    height: 150, 
+    types: ['Psychic'], 
+    abilities: ['Synchronize', 'Inner focus']
     }
-}
+  ];
+  pokemonList.forEach(function(pokemon){
+    if (pokemon.height >100){
+        document.write(pokemon.name + ':' + ' ' + 'height-' + ' ' + pokemon.height + 'cm' + '- Wow, that\'s big!<br>');
+    }else {
+        document.write(pokemon.name + ':' + ' ' + 'height-' + ' ' + pokemon.height + 'cm<br>');
+    }
+  })
