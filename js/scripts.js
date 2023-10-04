9ff4ee71d10d5948733c551b98853ece611bbc7f
tcederberg <trcederberg@gmail.com>
2023-10-03 20:40:42 -0400

    This commit wraps pokemonList array in IIFE.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,38 @@
-let pokemonList = [
-    {name: 'Squirtle', 
-    height: 50, 
-    types: ['Water'], 
-    abilities: ['Rain dish', 'Torrent']
-    },
-    {name: 'Vulpix', 
-    height: 60, 
-    types: ['Fire'], 
-    abilities: ['Flash fire', 'Drought']
-    },
-    {name: 'Alakazam', 
-    height: 150, 
-    types: ['Psychic'], 
-    abilities: ['Synchronize', 'Inner focus']
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {name: 'Squirtle', 
+        height: 50, 
+        types: ['Water'], 
+        abilities: ['Rain dish', 'Torrent']
+        },
+        {name: 'Vulpix', 
+        height: 60, 
+        types: ['Fire'], 
+        abilities: ['Flash fire', 'Drought']
+        },
+        {name: 'Alakazam', 
+        height: 150, 
+        types: ['Psychic'], 
+        abilities: ['Synchronize', 'Inner focus']
+        }
+    ];
+
+    function getAll () {
+        return pokemonList;
     }
-  ];
-  pokemonList.forEach(function(pokemon){
+    function add (pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    }
+})();
+pokemonRepository.getAll();
+pokemonRepository.add(item);
+
+pokemonList.forEach(function(pokemon){
     if (pokemon.height >100){
         document.write(pokemon.name + ':' + ' ' + 'height-' + ' ' + pokemon.height + 'cm' + '- Wow, that\'s big!<br>');
     }else {
