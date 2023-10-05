0a026b5d1fd04650e6beed47f9c58def34ab8b36
tcederberg <trcederberg@gmail.com>
2023-10-05 08:50:00 -0400

    This commit adds polyfill js files to index; apiUrl, loadList, loadDetails, and showDetails to js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,95 @@
 let pokemonRepository = (function () {
+    let pokemonList = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
+    function getAll () {
+        return pokemonList;
+    }
+    
+    function add (pokemon) {
+        if (
+            typeof pokemon === "object" &&
+            "name" in pokemon &&
+            "detailsUrl" in pokemon
+        ) {
+        pokemonList.push(pokemon);
+    } else {
+        console.log("pokemon is not correct");
+    }
+    }
+
+    function addListItem(pokemon){
+        let pokemonList = document.querySelector(".pokemon-list");
+        let listpokemon = document.createElemenet("li");
+        let button = document.createElement("button");
+        button.addEventListener('click', function(pokemon){
+            console.log(pokemon);
+        });
+        button.innerText = pokemon.name;
+        button.classList.add("button-class");
+        listpokemon.appendChild(button);
+        pokemonList.appendChild(listpokemon);
+        button.addEventListener("click", function(event) {
+            showDetails(pokemon);
+        })
+    }
+
+    function loadList() {
+        return fetch(apiUrl).then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            json.results.forEach(function (item) {
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+                console.log(pokemon);
+            });
+        }).catch(function (e) {
+            console.error(e);
+        });
+    }
+
+    function loadDetails(item) {
+        let url = item.detailsUrl;
+        return fetch(url).then(function (response) {
+            return response.json();
+        }).then(function (details) {
+            item.imageUrl = details.sprites.front_default;
+            item.height = details.height;
+            item.types = details.types;
+        }).catch(function (e) {
+            console.error(e);
+        });
+    }
+
+    function showDetails(item) {
+        pokemonRepository.loadDetails(item).then(function () {
+        console.log(item);
+        });
+    }
+
+    return {
+        getAll: getAll,
+        add: add,
+        addListItem: addListItem,
+        loadList: loadList,
+        loadDetails: loadDetails,
+        showDetails: showDetails
+    };
+})();
+
+pokemonRepository.loadList().then(function() {
+    pokemonRepository.getAll().forEach(function (pokemon) {
+        pokemonRepository.addListItem(pokemon);
+    });
+});
+
+
+
+/* Exercise 1.6 code
+let pokemonRepository = (function () {
     let repository = [
         {name: 'Squirtle', 
         height: 50, 
@@ -72,7 +163,7 @@ pokemonRepository.add({ name: "Pikachu", height: 40, types: ['Electric'], abilit
 pokemonRepository.getAll().forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
 });
-
+*/
 
 
 /* Exercise 1.5 code before DOM Manipulation and event listener.
