9f1d3af0add1725c56d8a043601a4ad3e7ec05b7
tcederberg <trcederberg@gmail.com>
2023-10-07 10:20:48 -0400

    This commit will upload to pokedex1.8.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,129 @@
+let pokemonRepository = (function () {
+    //make #modal-container a global variable
+    let modalContainer = document.querySelector('#modal-container');
+
+    //edit showDetails function to open the modal
+    function showDetails(pokemon) {
+        pokemonRepository.loadDetails(pokemon).then(function() { 
+            showModal(title, text);
+                //clear all existing modal content
+            modalContainer.innerHTML = '';
+
+                //create div element
+            let modal = document.createElement('div');
+            modal.classList.add('modal');
+        
+            //add the new modal content
+                //create close button element
+            let closeButtonElement = document.createElement('button');
+                //create button class for css styling
+            closeButtonElement.classList.add('modal-close');
+                //inner text in button to say Close
+            closeButtonElement.innerText = 'Close';
+                //if close is clicked hideModal will activate 
+            closeButtonElement.addEventListener('click', hideModal);
+        
+                //create title element 
+            let titleElement = document.createElement('h1');
+            titleElement.innerText = title;
+        
+            //create text element in p
+            let contentElement = document.createElement('p');
+            contentElement.innerText = text;
+
+            let container = document.querySelector('#image-container');
+            let myImage = document.createElement('img');
+            myImage.src = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+            
+            modalContainer.appendChild(myImage);
+                //append close button to modal
+            modal.appendChild(closeButtonElement);
+                //append title to modal
+            modal.appendChild(titleElement);
+                //append text to modal
+            modal.appendChild(contentElement);
+                //apend modal in parent modalContainer
+            modalContainer.appendChild(modal);
+            
+            modalContainer.classList.add('is-visible');
+            }
+        )
+    }
+        //enable close button with function hideModal
+        function hideModal() {
+        // we have defined modalContainer as a global variable
+        //let modalContainer = document.querySelector('#modal-container');
+            modalContainer.classList.remove('is-visible');
+
+        if (dialogPromiseReject) {
+            dialogPromiseReject();
+            dialogPromiseReject = null;
+        }
+        }
+
+        function showDialog(title, text) {
+            showModal(title, text);
+        
+            // We have defined modalContainer as a global variable
+            // let modalContainer = document.querySelector('#modal-container');
+        
+            // We want to add a confirm and cancel button to the modal
+            let modal = modalContainer.querySelector('.modal');
+        
+            let confirmButton = document.createElement('button');
+            confirmButton.classList.add('modal-confirm');
+            confirmButton.innerText = 'Confirm';
+        
+            let cancelButton = document.createElement('button');
+            cancelButton.classList.add('modal-cancel');
+            cancelButton.innerText = 'Cancel';
+        
+            modal.appendChild(confirmButton);
+            modal.appendChild(cancelButton);
+        
+            // We want to focus the confirmButton so that the user can simply press Enter
+            confirmButton.focus();
+            // Return a promise that resolves when confirmed, else rejects
+            return new Promise((resolve, reject) => {
+                cancelButton.addEventListener('click', hideModal);
+                confirmButton.addEventListener('click', () => {
+                dialogPromiseReject = null; // Reset this
+                hideModal();
+                resolve();
+                });
+            // This can be used to reject from other functions
+            dialogPromiseReject = reject;
+        });
+    }
+            //function to allow ESP-key to activate hideModal
+        window.addEventListener('keydown', (e) => {
+            if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+            hideModal();  
+            }
+        });
+            //function to allow the modal to close when clicked outside of modal on screen
+        modalContainer.addEventListener('click', (e) => {
+            // Since this is also triggered when clicking INSIDE the modal
+            // We only want to close if the user clicks directly on the overlay
+            let target = e.target;
+            if (target === modalContainer) {
+            hideModal();
+            }
+        }); 
+
+        document.querySelector('#show-modal').addEventListener('click', () => {
+            showModal('pokemon.name', 'pokemon.height');
+        });
+
+        document.querySelector('#show-dialog').addEventListener('click', () => {
+            showDialog('Confirm action', 'Are you sure you want to do this?').then(function() {
+            alert('confirmed!');
+            }, () => {
+            alert('not confirmed');
+            });
+})();
+
+
 /*Exercise 1.6 code
 //create an IIFE to encapsulate the code
 let pokemonRepository = (function () {
@@ -104,7 +230,7 @@ pokemonRepository.getAll().forEach(function (pokemon) {
 
 
     //Exercise 1.7 code still to be revised
-let pokemonRepository = (function () {
+/*let pokemonRepository = (function () {
     let pokemonList = [];
         //link API here
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
@@ -210,7 +336,7 @@ pokemonRepository.loadList().then(function() {
     pokemonRepository.getAll().forEach(function (pokemon) {
         pokemonRepository.addListItem(pokemon);
     }); //iterate through pokemon and call addListItem function
-});
+});*/
 
 
 
